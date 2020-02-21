309ad3cb4e958f3e5dbf5adddd24a1e4f5b36e37
maCSim161 <macsimtarnuev@mail.ru>
2020-02-21 11:24:41 +0900

    user

--- BugTracking/BugTracking/models.cs
+++ BugTracking/BugTracking/models.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.
 
 namespace BugTracking
 {
     class models
     {
+        enum UserStatus {User, Moderator, Admin};
+        public class User
+        {
+            [Key]
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Login { get; set; }
+            public string Password { get; set; }
+            public UserStatus Status { get; set;}
+            public string Mail { get; set; }
+            public int PhoneNumber { get; set; }
+
+        }
     }
 }
