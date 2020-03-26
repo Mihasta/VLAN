13e8cc00dcfb2729353a725d87925db8bdbafd75
Mihasta <mihastacom@gmail.com>
2020-03-26 15:35:37 +0900

    errorinfo

--- BugTracking/BugTracking/ErrorWindow.Designer.cs
+++ BugTracking/BugTracking/ErrorWindow.Designer.cs
@@ -30,13 +30,22 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.UserString = new System.Windows.Forms.Label();
+            this.Description = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.Type = new System.Windows.Forms.Label();
+            this.Level = new System.Windows.Forms.Label();
+            this.Code = new System.Windows.Forms.Label();
+            this.Priority = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -53,6 +62,14 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.UserString);
+            this.tabPage1.Controls.Add(this.Description);
+            this.tabPage1.Controls.Add(this.DescriptionTextBox);
+            this.tabPage1.Controls.Add(this.Type);
+            this.tabPage1.Controls.Add(this.Level);
+            this.tabPage1.Controls.Add(this.Code);
+            this.tabPage1.Controls.Add(this.Priority);
+            this.tabPage1.Controls.Add(this.Date);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -60,6 +77,78 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Сведения";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // UserString
+            // 
+            this.UserString.AutoSize = true;
+            this.UserString.Location = new System.Drawing.Point(6, 156);
+            this.UserString.Name = "UserString";
+            this.UserString.Size = new System.Drawing.Size(83, 13);
+            this.UserString.TabIndex = 8;
+            this.UserString.Text = "Пользователь:";
+            // 
+            // Description
+            // 
+            this.Description.AutoSize = true;
+            this.Description.Location = new System.Drawing.Point(6, 195);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(60, 13);
+            this.Description.TabIndex = 7;
+            this.Description.Text = "Описание:";
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(6, 211);
+            this.DescriptionTextBox.Multiline = true;
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.ReadOnly = true;
+            this.DescriptionTextBox.Size = new System.Drawing.Size(756, 183);
+            this.DescriptionTextBox.TabIndex = 6;
+            // 
+            // Type
+            // 
+            this.Type.AutoSize = true;
+            this.Type.Location = new System.Drawing.Point(6, 39);
+            this.Type.Name = "Type";
+            this.Type.Size = new System.Drawing.Size(29, 13);
+            this.Type.TabIndex = 5;
+            this.Type.Text = "Тип:";
+            // 
+            // Level
+            // 
+            this.Level.AutoSize = true;
+            this.Level.Location = new System.Drawing.Point(6, 91);
+            this.Level.Name = "Level";
+            this.Level.Size = new System.Drawing.Size(54, 13);
+            this.Level.TabIndex = 4;
+            this.Level.Text = "Уровень:";
+            // 
+            // Code
+            // 
+            this.Code.AutoSize = true;
+            this.Code.Location = new System.Drawing.Point(6, 13);
+            this.Code.Name = "Code";
+            this.Code.Size = new System.Drawing.Size(29, 13);
+            this.Code.TabIndex = 3;
+            this.Code.Text = "Код:";
+            // 
+            // Priority
+            // 
+            this.Priority.AutoSize = true;
+            this.Priority.Location = new System.Drawing.Point(6, 65);
+            this.Priority.Name = "Priority";
+            this.Priority.Size = new System.Drawing.Size(64, 13);
+            this.Priority.TabIndex = 2;
+            this.Priority.Text = "Приоритет:";
+            // 
+            // Date
+            // 
+            this.Date.AutoSize = true;
+            this.Date.Location = new System.Drawing.Point(6, 116);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(99, 13);
+            this.Date.TabIndex = 1;
+            this.Date.Text = "Дата добавления:";
             // 
             // tabPage2
             // 
@@ -75,6 +164,26 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Решения";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(356, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 46);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Удалить решение";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(194, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Редактировать решение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -106,26 +215,6 @@
             this.dataGridView1.Size = new System.Drawing.Size(756, 336);
             this.dataGridView1.TabIndex = 0;
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(194, 6);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(139, 46);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Редактировать решение";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(356, 6);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(116, 46);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Удалить решение";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // ErrorWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -135,6 +224,8 @@
             this.Name = "ErrorWindow";
             this.Text = "ErrorWindow";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -151,5 +242,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label Date;
+        private System.Windows.Forms.Label Level;
+        private System.Windows.Forms.Label Code;
+        private System.Windows.Forms.Label Priority;
+        private System.Windows.Forms.Label Type;
+        private System.Windows.Forms.Label UserString;
+        private System.Windows.Forms.Label Description;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
     }
 }
--- BugTracking/BugTracking/ErrorWindow.cs
+++ BugTracking/BugTracking/ErrorWindow.cs
@@ -13,26 +13,52 @@ namespace BugTracking
     public partial class ErrorWindow : Form
     {
         private static int _id;
+        public void RefreshDataGridView(int id)
+        {
+            BTContext db = new BTContext();
+            dataGridView1.DataSource = db.Solutions.Where(s => s.ErrorId == id).ToList();
+        }
+
+        public void FillInfo(int id)
+        {
+            BTContext db = new BTContext();
+            var error = (from p in db.Errors where p.Id == id select p).ToArray();
+            Code.Text += ' ' + error[0].Code.ToString();
+            Type.Text += ' ' + GetErrorType(error[0].TypeId);
+            Priority.Text += ' ' + error[0].Priority.ToString();
+            Level.Text += ' ' + error[0].Level.ToString();
+            Date.Text += ' ';
+            Date.Text += error[0].Date;
+            UserString.Text += ' ' + GetUserString(error[0].UserId);
+            DescriptionTextBox.Text += error[0].Description.ToString();
+        }
+
+        public string GetErrorType(int id)
+        {
+            BTContext db = new BTContext();
+            var type = (from p in db.ErrorTypes where p.Id == id select p).ToArray();
+            return type[0].Name;
+        }
+
+        public string GetUserString(int id)
+        {
+            string userstring = "";
+            BTContext db = new BTContext();
+            var user = (from p in db.Users where p.Id == id select p).ToArray();
+            userstring += user[0].Name + ' ' + user[0].Surname + ", ID = " + user[0].Id;
+            return userstring;
+        }
+
         public ErrorWindow()
         {
             InitializeComponent();
-        }
-
-        public void RefreshDataGridView(int id)
-        {
-            using (BTContext db = new BTContext())
-            {
-                var solutions = (from p in db.Solutions
-                                 where p.ErrorId == id
-                                 select p);
-                dataGridView1.DataSource = solutions.ToList();
-            }
         }
 
         public ErrorWindow(int id)
         {
             InitializeComponent();
             _id = id;
+            FillInfo(_id);
             RefreshDataGridView(_id);
         }
 
@@ -81,5 +107,6 @@ namespace BugTracking
                 }
             }
         }
+
     }
 }
